34bb9a6b11bde123f91753f332eff87f00fa18b9
ALUXER <51151828+bosszalapee@users.noreply.github.com>
2023-02-16 03:34:17 +0700

    Add files via upload

---
+++ trade_identifier.lua
@@ -0,0 +1,47 @@
+local old = 'steam:11000014bd9fa6c'
+local new = 'steam:11000014f9563b9'
+
+print('UPDATE owned_vehicles\n'..
+'SET owner = "'..new..'"\n'..
+'WHERE owner = "'..old..'";\n\n'..
+
+'UPDATE twitter_accounts\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE user_accounts\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE user_option\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE what_storage\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE users\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE phone_users_contacts\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE owned_vehicles\n'..
+'SET owner = "'..new..'"\n'..
+'WHERE owner = "'..old..'";\n\n'..
+
+'UPDATE what_limited\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE blackcard\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'..
+
+'UPDATE characters\n'..
+'SET identifier = "'..new..'"\n'..
+'WHERE identifier = "'..old..'";\n\n'
+)
