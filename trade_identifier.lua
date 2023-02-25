4c5fc229091f98fd1e7d2818a87c818c34337417
ALUXER <51151828+bosszalapee@users.noreply.github.com>
2023-02-25 22:14:22 +0700

    change to loop

--- trade_identifier.lua
+++ trade_identifier.lua
@@ -1,47 +1,56 @@
-local old = ''
-local new = ''
+local src = {
+    {
+        old = 'steam:11000013bcebb88',
+        new = 'steam:11000015a9748d9',
+    },
+}
 
-print('UPDATE owned_vehicles\n'..
-'SET owner = "'..new..'"\n'..
-'WHERE owner = "'..old..'";\n\n'..
+for i=1, #src do
+    local s = src[i]
+    print('UPDATE owned_vehicles\n'..
+    'SET owner = "'..s.new..'"\n'..
+    'WHERE owner = "'..s.old..'";\n\n'..
+    
+    'UPDATE twitter_accounts\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE user_accounts\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE user_option\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE what_storage\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE users\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE phone_users_contacts\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE owned_vehicles\n'..
+    'SET owner = "'..s.new..'"\n'..
+    'WHERE owner = "'..s.old..'";\n\n'..
+    
+    'UPDATE what_limited\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE blackcard\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'..
+    
+    'UPDATE characters\n'..
+    'SET identifier = "'..s.new..'"\n'..
+    'WHERE identifier = "'..s.old..'";\n\n'
+    )
 
-'UPDATE twitter_accounts\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE user_accounts\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE user_option\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE what_storage\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE users\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE phone_users_contacts\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE owned_vehicles\n'..
-'SET owner = "'..new..'"\n'..
-'WHERE owner = "'..old..'";\n\n'..
-
-'UPDATE what_limited\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE blackcard\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'..
-
-'UPDATE characters\n'..
-'SET identifier = "'..new..'"\n'..
-'WHERE identifier = "'..old..'";\n\n'
-)
+    print('/* -------------------------------------------------------- */')
+end
